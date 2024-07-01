f7bc240ec6fb5ec696d94d8300349cbe87b92d6d
Marcos Gomes <marcosgomesdev@gmail.com>
2024-07-01 12:39:50 -0300

    feat: add auth server action

---
+++ server-actions/auth.action.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { LoginForm } from "@/app/(withoutLayout)/login/components/Form";
+import { redirect } from "next/navigation";
+import { AuthService } from "../services/auth.service";
+
+export async function loginAction(form: LoginForm) {
+  const { email, password, redirect_to } = form;
+
+  const authService = new AuthService();
+
+  const error = await authService.login({ email, password });
+
+  if (error) {
+    return {
+      error: error,
+    };
+  }
+
+  redirect(redirect_to || "/");
+}
+
+export async function logoutAction() {
+  const authService = new AuthService();
+  authService.logout();
+  redirect("/login");
+}
